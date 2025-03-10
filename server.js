4774391c71dc1953bbe844aae3110254fddd0286
gg <73786408+n14-py@users.noreply.github.com>
2025-03-10 13:42:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const http = require('http'); // Paquete necesario para hacer una petición HTTP a sí mismo
 const app = express();
 
 // Servir el archivo HTML
@@ -14,12 +13,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor web en línea en el puerto ${port}`);
   
-  // Hacer ping al servidor cada 5 minutos (300000 ms)
-  setInterval(() => {
-    http.get(`http://localhost:${port}`, (response) => {
-      console.log(`Ping al servidor exitoso: ${response.statusCode}`);
-    }).on('error', (e) => {
-      console.error('Error al hacer ping al servidor:', e);
-    });
-  }, 5 * 60 * 1000); // 5 minutos en milisegundos
-});
+ 
