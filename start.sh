bf177b67655ba95b9a938d8ba956012fde85406d
gg <73786408+n14-py@users.noreply.github.com>
2025-03-16 20:07:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Montar drives con rclone
-rclone mount gdrive_videos: /mnt/gdrive_videos --daemon
-rclone mount gdrive_sonidos: /mnt/gdrive_sonidos --daemon
-rclone mount gdrive_musica: /mnt/gdrive_musica --daemon
+# Montar drives usando la variable de entorno
+rclone mount --config $RCLONE_CONFIG gdrive_videos: /mnt/gdrive_videos --daemon
+rclone mount --config $RCLONE_CONFIG gdrive_sonidos: /mnt/gdrive_sonidos --daemon
+rclone mount --config $RCLONE_CONFIG gdrive_musica: /mnt/gdrive_musica --daemon
 
-# Esperar montaje
-sleep 5
+sleep 10  # Esperar montaje
 
-# Iniciar streaming
 python main.py
