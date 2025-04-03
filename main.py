52feb59f66a89454cd325fe044a4450f7ebfc488
gg <73786408+n14-py@users.noreply.github.com>
2025-04-03 01:59:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ class YouTubeManager:
             logging.error(f"Error creación transmisión: {str(e)}")
             return None
     
-def iniciar_transmision(self, broadcast_id):
+    def iniciar_transmision(self, broadcast_id):
         max_intentos = 5
         espera_base = 15  # Segundos
         
